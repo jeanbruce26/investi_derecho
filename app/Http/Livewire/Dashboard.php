c82a2a89a5cefd8cddbd066f1fdcc2255de8966b
Jamt Mendoza <43344540+pupilojamt@users.noreply.github.com>
2022-10-14 08:55:44 -0500

    TABLA REPORTE PROYECTO POR PERSONA TERMINADO

--- app/Http/Livewire/Dashboard.php
+++ app/Http/Livewire/Dashboard.php
@@ -17,7 +17,7 @@ class Dashboard extends Component
     {
         $id = $this->persona_id;
         $search = $this->buscar;
-        $proyecto = PersonaProyecto::join('proyecto','persona_proyecto.proyecto_id','=','proyecto.proyecto_id')->join('categoria_proyecto','proyecto.categoria_proyecto_id','=','categoria_proyecto.categoria_proyecto_id')->where(function($query) use ($id){$query->where('persona_proyecto.persona_id',$id);})->where(function($query) use ($search){$query->where('proyecto.proyecto_titulo','LIKE',"%{$search}%")->orWhere('categoria_proyecto.categoria_proyecto','LIKE',"%{$search}%")->orWhere('proyecto.proyecto_financiamiento','LIKE',"%{$search}%");})->orderBy('proyecto.proyecto_id','ASC')->paginate(10);
+        $proyecto = PersonaProyecto::join('proyecto','persona_proyecto.proyecto_id','=','proyecto.proyecto_id')->join('categoria_proyecto','proyecto.categoria_proyecto_id','=','categoria_proyecto.categoria_proyecto_id')->join('participante_proyecto','persona_proyecto.participante_proyecto_id','=','participante_proyecto.participante_proyecto_id')->where(function($query) use ($id){$query->where('persona_proyecto.persona_id',$id);})->where(function($query) use ($search){$query->where('proyecto.proyecto_titulo','LIKE',"%{$search}%")->orWhere('categoria_proyecto.categoria_proyecto','LIKE',"%{$search}%")->orWhere('proyecto.proyecto_financiamiento','LIKE',"%{$search}%")->orWhere('participante_proyecto.participante_proyecto','LIKE',"%{$search}%");})->orderBy('proyecto.proyecto_id','ASC')->paginate(10);
 
         return view('livewire.dashboard', [
             'proyecto' => $proyecto,
